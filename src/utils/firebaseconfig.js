5e31cd8e8ea68a0c391f0ef0ebe15f3b0c4d6bd8
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-04-29 12:35:44 -0600

    firebase?

---
+++ src/utils/firebaseconfig.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyDB4s0GJ1Lzf_u4nOKS-prCt04VN70rOE0",
+  authDomain: "ski-vue.firebaseapp.com",
+  databaseURL: "https://ski-vue.firebaseio.com",
+  projectId: "ski-vue",
+  storageBucket: "ski-vue.appspot.com",
+  messagingSenderId: "196848815413"
+}
